cb1c36c8aefead3ca7db9c91d7750fad1aeea8aa
Nenpotato <59008922+Sod3n@users.noreply.github.com>
2025-10-27 00:44:19 -0700

    WIP

---
+++ Assets/Light and controller/Scripts/SaveSystem.cs
@@ -0,0 +1,15 @@
+namespace Light_and_controller.Scripts
+{
+    public static class SaveSystem
+    {
+        public static void SaveGame()
+        {
+            // Level
+        }
+
+        public static void LoadGame()
+        {
+            // Level
+        }
+    }
+}
