5bb72d889f785c8253d80c4844997132d11490a8
dantisit <dantist1231@gmail.com>
2025-10-28 01:09:29 +0700

    LightRay2

--- Assets/Light and controller/Scenes/LightRayScaller.cs
+++ Assets/Light and controller/Scenes/LightRayScaller.cs
@@ -4,13 +4,52 @@ using UnityEngine.Rendering.Universal;
 
 public class LightRayScaller : MonoBehaviour
 {
-    [SerializeField] private Light2D Light;
-    [SerializeField] private Vector3[] Shape;
+    [SerializeField] private Collider2D collider;
+    [SerializeField] private Light2D light;
+    [SerializeField] private Vector3[] shape;
     public float speed;
+    
     void Update()
     {
-        Debug.Log(Time.deltaTime * speed * Shape[0].x);
-        Shape[0].x += Time.deltaTime * speed;
-        Light.SetShapePath(Shape);
+        // Изменяем форму света - обе точки по X
+        shape[0].x += Time.deltaTime * speed;
+        if (shape.Length > 1)
+        {
+            shape[1].x += Time.deltaTime * speed;
+        }
+        light.SetShapePath(shape);
+
+        // Обновляем коллайдер
+        UpdateCollider();
+    }
+
+    private void UpdateCollider()
+    {
+        if (collider is BoxCollider2D boxCollider)
+        {
+            // Вычисляем границы формы света
+            Vector2 min = shape[0];
+            Vector2 max = shape[0];
+            
+            for (int i = 1; i < shape.Length; i++)
+            {
+                min = Vector2.Min(min, shape[i]);
+                max = Vector2.Max(max, shape[i]);
+            }
+
+            // Устанавливаем размер и центр коллайдера
+            Vector2 size = max - min;
+            boxCollider.size = size;
+            boxCollider.offset = min + size / 2f;
+        }
+    }
+
+    // Для инициализации в редакторе
+    private void OnValidate()
+    {
+        if (shape != null && shape.Length > 0 && light != null)
+        {
+            UpdateCollider();
+        }
     }
 }
